d1df0c33da22ee4fd3c828431bf46ed826402a20
insu <wjddlstn0614@naver.com>
2019-03-26 14:02:39 +0900

    수정렬하기3 및 readme 파일 수정

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ https://backlog.com/git-tutorial/kr/intro/intro2_1.html
 소팅 방법 6가지를 다 써보는 것을 목적으로 하고 있습니다.  
 selection,bubble,insert,merge,quick,heap sort를 전부 구현해서 올려주시기 바랍니다.  
 
-버블 소트 https://www.acmicpc.net/problem/1517
-버블소트 너무 어려운듯
+버블 소트 https://www.acmicpc.net/problem/1517  
+버블소트 너무 어려운듯  
 나무 조각 https://www.acmicpc.net/problem/2947  
 이중 연결 리스트 https://www.acmicpc.net/problem/3045  
 수 정렬하기 3 https://www.acmicpc.net/problem/10989  
---
+++ 정인수/10989_수정렬하기3.cpp
@@ -0,0 +1,27 @@
+//수정렬하기
+//10989번
+//https://www.acmicpc.net/problem/10989
+//문제풀이 counting sort를 사용한풀이
+
+#include  <stdio.h>
+int a[10000];
+
+int main () {
+    
+    int size;
+    int temp;
+    scanf("%d",&size);
+    for(int i=0;i<size;i++){
+        scanf("%d",&temp);
+        a[temp-1]++;
+    }
+    
+    for(int i=0;i<size;i++){
+        if(a[i]!=0){
+            for(int j=0;j<a[i];j++){
+                printf("%d\n",i+1);
+            }
+        }
+    }
+    return 0;
+}
