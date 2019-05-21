0b5e89a9e50ea0972de084fd6e738d0acb7dd8b0
insu Jeong <wjddlstn0614@naver.com>
2019-05-22 00:54:25 +0900

    최단경로 문제 풀이 추가

--- 정인수/6week_algorithm/1753.cpp
+++ 정인수/6week_algorithm/1753.cpp
@@ -9,10 +9,43 @@
 
 using namespace std;
 
-vector< pair<int,int> > abj[20001];
+vector< pair<int,int> > adj[20001];
 
 vector<int> dijkstra(int start,int V, int E){
+    priority_queue< pair<int, int> > pq;
+
+    //각 정점을 최대치로 초기화 하여 준다.
+    //start로부터 각 정점까지의 최단거리를 저장하는 배열(vector)이다.
+    vector<int> dist(V,INT_MAX);
+    dist[start] = 0;
     
+    //프라이어리티 큐에 처음 노드를 넣어준다.
+    pq.push(make_pair(0,start));
+
+    int here = start;
+
+    while(!pq.empty()){
+        //priority queue에서 top에 있는 값을 추출해낸다.
+        int cost = -pq.top().first;     //cost는 가장 작은 값이 top이 있어야하기 때문에 -로 저장해두어서 -를 붙여 양수로 바꿔줌
+        int here = pq.top().second;    //there은 pq에 간선이 가르키는 정점이다.
+
+        //추출이 끝난 요소를 pop해줘서 queue에서 제외하여 준다.
+        pq.pop();
+
+        for(int i=0;i<adj[here].size();i++){
+
+            int nextdist = cost + adj[here][i].second;
+            int there = adj[here][i].first;
+
+            if(dist[there] > nextdist){
+                dist[there] = nextdist;
+                pq.push(make_pair(-dist[there],there));
+            }
+        }
+
+    }
+
+    return dist;
 }
 
 int main () {
@@ -29,7 +62,7 @@ int main () {
 
     vector<int> ans = dijkstra(start,V,E);
 
-    for(int i=0;i<V;i++){
+    for(int i=1;i<V;i++){
         ans[i] == INT_MAX ? printf("INF\n") :printf("%d\n",ans[i]);
     }
 
