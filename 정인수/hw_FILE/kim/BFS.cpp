5d3a573ac022d489ab4951ba8d834d9c98bebdaf
insu Jeong <wjddlstn0614@naver.com>
2019-05-30 16:46:37 +0900

    BFS complete

--- 정인수/hw_FILE/kim/BFS.cpp
+++ 정인수/hw_FILE/kim/BFS.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <queue>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main () {
@@ -9,6 +10,7 @@ int main () {
     int from,to;
     scanf("%d %d",&nodeNum,&ajdNum);
     vector<bool> visit(nodeNum+1,false);
+    
     vector<int> adj[nodeNum+1];
     for(int i=0;i<ajdNum;i++){
         scanf("%d %d",&from,&to);
@@ -17,19 +19,28 @@ int main () {
     }
     scanf("%d",&startNode);
 
+    for(int i=1;i<=nodeNum;i++){
+        sort(adj[i].begin(),adj[i].end());
+    }
+
     queue<int> q;
     q.push(startNode);
+    visit[startNode] = true; 
+    int count =0;
     while(!q.empty()){
+        count++;
         printf("%d ",q.front());
         int here = q.front();
         q.pop();
         for(int i=0;i<adj[here].size();i++){
             int there = adj[here][i];
-            if(!visit[there])
+            if(!visit[there]){
                 visit[there] = true;
                 q.push(there);
+                //printf("queue.push(there) there = %d,queue.size() = %d\n",there,q.size());                
+            }
         }
     }
-
+    
     return 0;
 }
