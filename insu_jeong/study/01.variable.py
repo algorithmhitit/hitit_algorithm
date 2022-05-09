7381925a6d7d2280ec33a33777ef3c69faf50d4d
insujeong <wjddlstn0614@naver.com>
2022-05-09 15:26:02 +0900

    python test

---
+++ insu_jeong/study/01.variable.py
@@ -0,0 +1,18 @@
+variable1 = 100
+variable2 = 3.14
+variable3 = -200
+variable4 = 1.2 + 3.4j
+variable5 = 'This is Python'
+
+variable6 = True
+variable7 = float(variable1)
+variable8 = int(variable2)
+
+print('variable1 = ' , variable1, type(variable1))
+print('variable2 = ' , variable2, type(variable2))
+print('variable3 = ' , variable3, type(variable3))
+print('variable4 = ' , variable4, type(variable4))
+print('variable5 = ' , variable5, type(variable5))
+print('variable6 = ' , variable6, type(variable6))
+print('variable7 = ' , variable7, type(variable7))
+print('variable8 = ' , variable8, type(variable8))
